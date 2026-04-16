01e9291ab7113ccf1b3c2db9a8ac24e598e9e561
Pratik Zende <pratikzende882002@gmail.com>
2026-04-16 12:46:02 +0530

    added var parameter in jenkins file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123927200"
+    bucket = "my-terraform-state-bucket-pratik123432"
     key = "class"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
